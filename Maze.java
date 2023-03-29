6db7a45d73b153dab007f1054b38b5060cf7bae5
srubaru <75611842+srubaru@users.noreply.github.com>
2023-03-29 15:42:40 +0000

    Adding starter code

---
+++ Maze.java
@@ -0,0 +1,9 @@
+class Maze{
+    private int size;
+    private int[][] nodes;
+    private Graph graph;
+
+    public static void main(String args[]){
+        System.out.println("Welcome to maze");
+    }
+}
